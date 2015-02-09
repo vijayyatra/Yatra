883ed5b74461a5e98e458cf9f8b6ed6014227e4e
amitkumar3968 <appsbackup@yatra.com>
2015-02-09 16:59:10 +0530

    Initial Commit

---
+++ demoGit/DemoAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DemoAppDelegate.h
+//  demoGit
+//
+//  Created by pankaj on 2/9/15.
+//  Copyright (c) 2015 Vijay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DemoAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
